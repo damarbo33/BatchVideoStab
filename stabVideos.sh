f6e33e9c489e3ffbff98a3fa0068c8a901e5e8e7
Daniel Marco <damarbo33@gmail.com>
2018-04-27 12:36:55 +0200

    improved sharpen effect and examples to crop and h265
    The decimal numbers of unsharp represent resasonable values between -1.5 to 1.5. Positive to sharpen and negative to soften
    the image.
    
    unsharp=5:5:1.0:5:5:1.0

--- stabVideos.sh
+++ stabVideos.sh
@@ -26,6 +26,21 @@ do
   ffmpeg2 -i "$videofile" -vf vidstabdetect=stepsize=32:shakiness=10:accuracy=10:result="$logvideofile" -f null - 
   
   echo "Procesando pase 2 para: "${entry##*/}
-  ffmpeg2 -i "$videofile" -vf vidstabtransform=input="$logvideofile":zoom=0:smoothing=10,unsharp=5:5:0.8:3:3:0.4 -vcodec libx264 -tune film -acodec copy -preset slow "$videofileNoExt"
+  ffmpeg2 -i "$videofile" -vf vidstabtransform=input="$logvideofile":zoom=0:smoothing=10,unsharp=5:5:1.0:5:5:1.0 -vcodec libx264 -tune film -acodec copy -preset slow "$videofileNoExt"
+  
+  ##########################################
+  #example to add cropping after the stabilitzation process. Very useful to achieve best quality 
+  #if we record in 1440p and we want to get rid of 
+  #some fake borders the vidstab plugin generates. (1440-1080)/2 = 240
+  #
+  #ffmpeg2 -i "$videofile" -vf vidstabtransform=input="$logvideofile":zoom=0:smoothing=10,unsharp=5:5:1.0:5:5:1.0,crop=1920:1080:0:240 -vcodec libx264 -tune film -acodec copy -preset slow "$videofileNoExt"
+  ##########################################
+  
+  ##########################################
+  #example to recode un h265
+  #
+  #ffmpeg2 -i "$videofile" -vf vidstabtransform=input="$logvideofile":zoom=0:smoothing=10,unsharp=5:5:1.0:5:5:1.0 -vcodec libx265 -tune zerolatency -acodec copy -preset fast "$videofileNoExt"
+  ##########################################
+  
 
 done
